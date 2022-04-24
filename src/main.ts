68471975e3fee3e4194597f920f0e75c249274c5
Stuart Huggins <stuhuggins@gmail.com>
2022-04-24 23:14:37 +0100

    set up shell root component

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 ﻿/// <reference types="aurelia-loader-webpack/src/webpack-hot-interface"/>
 // we want font-awesome to load as soon as possible to show the fa-spinner
-import '../static/styles.css';
-import 'font-awesome/css/font-awesome.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import { Aurelia } from 'aurelia-framework';
-import { PLATFORM } from 'aurelia-pal';
-import * as Bluebird from 'bluebird';
+import "../static/styles.css";
+import "font-awesome/css/font-awesome.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Aurelia } from "aurelia-framework";
+import { PLATFORM } from "aurelia-pal";
+import * as Bluebird from "bluebird";
 
 // remove out if you don't want a Promise polyfill (remove also from webpack.config.js)
 Bluebird.config({ warnings: { wForgottenReturn: false } });
 
 export async function configure(aurelia: Aurelia) {
-  aurelia.use
-    .standardConfiguration()
-    .developmentLogging();
+  aurelia.use.standardConfiguration().developmentLogging();
 
   // Uncomment the line below to enable animation.
   // aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
@@ -23,5 +21,5 @@ export async function configure(aurelia: Aurelia) {
   // aurelia.use.plugin(PLATFORM.moduleName('aurelia-html-import-template-loader'));
 
   await aurelia.start();
-  await aurelia.setRoot(PLATFORM.moduleName('app'));
+  await aurelia.setRoot(PLATFORM.moduleName("shell"));
 }
